5a65339b586463c721f694907082ec0bd73e00dd
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-02-09 21:24:40 -0800

    leap added

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -4,27 +4,26 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;  
 using System;
+using Leap;
 
 public class GameController : MonoBehaviour {
 	public GameObject Gate_Prefab;
 	private List<Vector3> center_points = new List<Vector3>();
 	private List<Vector3> right_vector = new List<Vector3>();
 	private List<Vector3> up_vector = new List<Vector3> ();
+	public HandController hand;
 	// Use this for initialization
 	void Start () {
 		Debug.Log(Load("/Users/ruiqingqiu/CSE165/Assets/test1.txt"));
 		//After loading the file, add gate based on the point is at
 		for(int i = 0; i < center_points.Count; i++){
-			//Create gate at different locations
-			Vector3 v1 = new Vector3(0,1,0);
-			Vector3 v2 = right_vector[i];
-			Vector3 a = Vector3.Cross(v1, v2);
-			double w = Math.Sqrt((v1.sqrMagnitude) * (v2.sqrMagnitude)) + Vector3.Dot(v1, v2);
-			float w1 = (float)w;
-			Quaternion q = new Quaternion(a.x, a.y, a.z, w1);
-			Debug.Log ("quaternion " + q);
-			//Create the gate
-			GameObject gate = (GameObject) Instantiate(Gate_Prefab, center_points[i], q);
+			Vector3 up = up_vector[i];
+			Vector3 right = right_vector[i];
+			Vector3 location = center_points[i];
+			location.y = location.y - 2.5f;			
+			GameObject gate = (GameObject) Instantiate(Gate_Prefab, location, Quaternion.FromToRotation(new Vector3(0,1,0), up));
+
+			//GameObject gate = (GameObject) Instantiate(Gate_Prefab, location,Quaternion.AngleAxis(a, aor));
 		}
 		/*
 		for (var y = 0; y < gridY; y++) {
@@ -37,7 +36,6 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 	private bool Load(string fileName)
 	{
@@ -72,9 +70,12 @@ public class GameController : MonoBehaviour {
 							center_points.Add(new Vector3(float.Parse(entries[0].Remove(0,2)),
 							                             float.Parse(entries[1].Remove(0,2)),
 							                             float.Parse(entries[2].Remove(0,2))));
+							//Add right vector to our list
 							right_vector.Add(new Vector3(float.Parse(entries[3].Remove(0,2)),
 							                             float.Parse(entries[4].Remove(0,2)),
 							                             float.Parse(entries[5].Remove(0,2))));
+
+							//Add up vector to our list
 							up_vector.Add(new Vector3(float.Parse(entries[6].Remove(0,2)),
 							                          float.Parse(entries[7].Remove(0,2)),
 							                          float.Parse(entries[8].Remove(0,2))));
---
+++ Assets/LeapMotion/Scripts/Hands/RiggedHand.cs
@@ -0,0 +1,48 @@
+﻿/******************************************************************************\
+* Copyright (C) Leap Motion, Inc. 2011-2014.                                   *
+* Leap Motion proprietary. Licensed under Apache 2.0                           *
+* Available at http://www.apache.org/licenses/LICENSE-2.0.html                 *
+\******************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+using Leap;
+
+// Class to setup a rigged hand based on a model.
+public class RiggedHand : HandModel {
+
+  public Transform palm;
+  public GameObject spaceship;
+  public Transform foreArm;
+
+  public Vector3 modelFingerPointing = Vector3.forward;
+  public Vector3 modelPalmFacing = -Vector3.up;
+
+  public override void InitHand() {
+    UpdateHand();
+  }
+
+  public Quaternion Reorientation() {
+    return Quaternion.Inverse(Quaternion.LookRotation(modelFingerPointing, -modelPalmFacing));
+  }
+
+  public override void UpdateHand() {
+    if (palm != null) {
+			//Modify spaceship's position
+	Vector3 position = GetPalmPosition();
+	Debug.Log (position);
+	position.Scale(new Vector3(5,5,5));
+	spaceship.transform.position = position;
+      palm.position = new Vector3(0,-300,0);
+      palm.rotation = GetPalmRotation() * Reorientation();
+    }
+
+    if (foreArm != null)
+      foreArm.rotation = GetArmRotation() * Reorientation();
+
+    for (int i = 0; i < fingers.Length; ++i) {
+      if (fingers[i] != null)
+        fingers[i].UpdateFinger();
+    }
+  }
+}
